80bb1b1b247aba29273cb538b4c7f9a0deca9f00
Paul K George <paulkottalil20@gmail.com>
2020-08-03 00:10:37 +0530

    initial commint

---
+++ course/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    subjects = models.ManyToManyField(Subject)
+    course_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name + '-'+ self.course_type
+
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    max_credits = models.IntegerField(default=0)
+    hours = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
